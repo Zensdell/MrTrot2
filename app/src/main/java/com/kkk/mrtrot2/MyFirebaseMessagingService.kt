9f9807507e302dd3a616b4f61ae9202ecb2d82e7
Kunote <rndudah754@gmail.com>
2022-09-21 20:46:07 +0900

    파이어베이스 푸시알람 작성

---
+++ app/src/main/java/com/kkk/mrtrot2/MyFirebaseMessagingService.kt
@@ -0,0 +1,70 @@
+package com.kkk.mrtrot2
+
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+const val channelName="package com.kkk.mrtrot2"
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if(remoteMessage.getNotification() !=null){
+            generateNotification(remoteMessage.notification!!.title!!,remoteMessage.notification!!.body!!)
+        }
+    }
+    @SuppressLint("RemoteViewLayout")
+    fun getRemoteView (title: String,message: String) : RemoteViews{
+        val remoteView = RemoteViews("package com.kkk.mrtrot2",R.layout.notification)
+
+        remoteView.setTextViewText(R.id.title,title)
+        remoteView.setTextViewText(R.id.message,message)
+        remoteView.setImageViewResource(R.id.app_logo,R.drawable.mr2icon)
+
+        return remoteView
+    }
+
+    fun generateNotification(title:String,message:String){
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT)
+        val channelId = "$packageName-${getString(R.string.app_name)}"
+
+        var builder : NotificationCompat.Builder=NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.mr2icon)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+            .setContentTitle("미스터트롯2 응원하기")
+
+        builder.priority=NotificationCompat.PRIORITY_HIGH
+
+        builder=builder.setContent(getRemoteView(title,message))
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val notificationChannel = NotificationChannel(channelId, channelName,NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        notificationManager.notify(0,builder.build())
+
+    }
+}
